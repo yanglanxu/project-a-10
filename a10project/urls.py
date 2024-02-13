26121d880d08097774458f6a2ea023e73c0e4168
Joel Eldo <xxq5gd@virginia.edu>
2024-02-13 16:12:27 -0500

    Set up urls.py for a10app and added the a10app path to the project urls.py

--- a10project/urls.py
+++ a10project/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('a10app/', include("a10app.urls")),
     path('admin/', admin.site.urls),
 ]
