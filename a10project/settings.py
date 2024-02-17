d468c8e517d57a89423064c740c639ab99b0e177
Joel Eldo <xxq5gd@virginia.edu>
2024-02-17 15:55:44 -0500

    Further updates to settings

--- a10project/settings.py
+++ a10project/settings.py
@@ -80,21 +80,14 @@ WSGI_APPLICATION = 'a10project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-if IS_HEROKU_APP:
-    DATABASES = {
-        "default": dj_database_url.config(
-            conn_max_age=600,
-            conn_health_checks=True,
-            ssl_require=True,
-        ),
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 # Password validation
