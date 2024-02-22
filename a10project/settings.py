3300a4c9a75fa1786c3414b9cb3c9764affe38e5
Joel Eldo <xxq5gd@virginia.edu>
2024-02-22 13:20:37 -0500

    Added local vs hosted database, changed url so that blank url leads to a10app

--- a10project/settings.py
+++ a10project/settings.py
@@ -83,9 +83,14 @@ WSGI_APPLICATION = 'a10project.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+"default": dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+        ),
+    "local": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
--- a10project/urls.py
+++ a10project/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('a10app/', include("a10app.urls")),
+    path('', include("a10app.urls")),
     path('admin/', admin.site.urls),
 ]
