b5f4ae380da6445bc847f4c3b7d8cab75408e5ab
Joel Eldo <xxq5gd@virginia.edu>
2024-02-23 14:00:30 -0500

    Modified Databases instantiation to not cause error locally

--- a10project/settings.py
+++ a10project/settings.py
@@ -85,16 +85,17 @@ WSGI_APPLICATION = 'a10project.wsgi.application'
 
 
 DATABASES = {
-"default": dj_database_url.config(
-            conn_max_age=600,
-            conn_health_checks=True,
-            ssl_require=True,
-        ),
-    "local": {
+    "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+if IS_HEROKU_APP:
+    DATABASES = dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+        )
 
 
 # Password validation
