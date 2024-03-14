e7f33c615837e0b771523b1f06d7e53e71833064
Joel Eldo <xxq5gd@virginia.edu>
2024-03-14 15:33:21 -0400

    Set up report and report file models

--- a10app/models.py
+++ a10app/models.py
@@ -8,3 +8,12 @@ class Users(models.Model):
 
     def __str__(self):
         return self.userName
+
+class Report(models.Model):
+    title = models.TextField(blank=True, max_length=500)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    text = models.TextField(blank=True, max_length=2000)
+
+class ReportFile(models.Model):
+    file = models.FileField(upload_to="media")
+    feed = models.ForeignKey(Report, on_delete=models.SET_NULL)
