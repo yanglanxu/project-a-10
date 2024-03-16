80363fccc281b049c339a9b3d59830f311a6218f
Joel Eldo <xxq5gd@virginia.edu>
2024-03-16 14:06:40 -0400

    Removed welcome and check, which did not serve much purpose

--- a10app/urls.py
+++ a10app/urls.py
@@ -6,7 +6,6 @@ from . import views
 app_name = "a10app"
 urlpatterns = [
     path("", views.index),
-    path("welcome", views.welcome),
     path("logout", views.logout),
     path("report", views.ReportFormView.as_view()),
     # path("welcome", views.isAdmin)
